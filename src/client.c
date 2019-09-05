ffce4e241dbac481cb153cbb653b3d834d028da2
Bartłomiej Kizielewicz <bartlomiej.kizielewicz@tieto.com>
2019-09-05 14:30:52 +0000

    functions in client.c

--- src/client.c
+++ src/client.c
@@ -10,42 +10,60 @@
 #define PORT 8080
 #define MAXLINE 1024
 
-// Driver code
-int main(){
+// Create socket
+void create_socket(int *sockfd){
 
-	int sockfd;
-	char buffer[MAXLINE];
-	char *hello = "Hello form client";
-	struct sockaddr_in servaddr;
-
-	// Creating socket file descriptor
-	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+	*sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(*sockfd < 0){
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
+}
+
+// Filling server information
+void set_info_server(struct sockaddr_in *servaddr){
+
+	servaddr->sin_family = AF_INET;
+	servaddr->sin_port = htons(PORT); //htons - convert values between host and network byte order
+	servaddr->sin_addr.s_addr = INADDR_ANY;
+
+}
+
+// Chat between client and server
+void chat_client(int *sockfd, struct sockaddr_in *servaddr){
+
+	char *string = malloc(sizeof(char) * 1024);
+	int n, len;
+	char *hello = "Hello from client";
+	char buffer[MAXLINE];
+	do{
+                scanf("%s", string);
+                sendto(*sockfd, (const char*)string, strlen(string), MSG_CONFIRM, (const struct sockaddr *) servaddr, sizeof(*servaddr));
+                printf("Hello message sent. \n");
+                if(strcmp(string, "exit\0") == 0)
+                        break;
+                n = recvfrom(*sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) servaddr, &len);
+                buffer[n] = '\0';
+                printf("Server : %s\n", buffer);
+        }while(string);
+        close(*sockfd);
+        free(string);
+
+}
+
+// Driver main
+int main(){
+
+	int sockfd;
+	struct sockaddr_in servaddr;
+
+	create_socket(&sockfd);
 
 	memset(&servaddr, 0, sizeof(servaddr));
 
-	// Filling server information
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(PORT);
-	servaddr.sin_addr.s_addr = INADDR_ANY;
+	set_info_server(&servaddr);
 
-	int n, len;
+	chat_client(&sockfd, &servaddr);
 
-	char *string = malloc(sizeof(char) * 1024);
-
-	do{
-		scanf("%s", string);
-		sendto(sockfd, (const char*)string, strlen(string), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-		printf("Hello message sent. \n");
-		if(strcmp(string, "exit\0") == 0)
-			break;
-		n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-		buffer[n] = '\0';
-		printf("Server : %s\n", buffer);
-	}while(string);
-	close(sockfd);
-	free(string);
 	return 0;
 }
