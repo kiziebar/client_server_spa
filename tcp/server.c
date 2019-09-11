e2baf0836018fbe98899093d22523d3bbe762a17
kiziebar <bartlomiej.kizielewicz@tieto.com>
2019-09-11 10:19:29 +0200

    server.c added communication with clients

--- tcp/server.c
+++ tcp/server.c
@@ -7,22 +7,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/epoll.h>
+#include <fcntl.h>
 #define MAX_EVENTS 5
 #define READ_SIZE 10
 #define MAX 80
-#define PORT 8888
+#define PORT 8123
 #define SA struct sockaddr
+
+int non_block(int sockfd)
+{
+    int flags, s;
+    flags = fcntl(sockfd, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, flags);
+    return 0;
+}
 
 // Driver function
 int main()
 {
-    int sockfd, connfd, len, event_count;
+    int sockfd, connfd, len, event_count, new_fd;
     size_t bytes_read;
     ssize_t count;
     char read_buffer[READ_SIZE + 1];
-    struct sockaddr_in servaddr, cli;
+    struct sockaddr_in servaddr, client;
     struct epoll_event event, events[MAX_EVENTS];
     int epoll_fd = epoll_create(3);
+    socklen_t addr_size;
 
     // checking fd epoll
     if(epoll_fd == -1)
@@ -61,7 +72,7 @@ int main()
     }
     else
         printf("Server listening..\n");
-    event.events = EPOLLIN;
+    event.events = EPOLLIN | EPOLLET;
     event.data.fd = sockfd;
 
     if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockfd, &event))
@@ -75,10 +86,30 @@ int main()
         event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 3000);
         for(int i = 0; i < event_count; i++)
         {
-                 count = read(events[i].data.fd, read_buffer, sizeof(read_buffer)-1);
-                 read_buffer[count] = '\0';
-                 printf("%s \n", read_buffer);
-	 	 printf("jestem");
+                 if(events[i].data.fd == sockfd)
+                 {
+                     addr_size = sizeof(client);
+                     new_fd = accept(events[i].data.fd, (struct sockaddr *) &client, &addr_size);
+                     if(new_fd == -1)
+                         printf("ERROR NEW_FD");
+                     printf("New connection on server...\n");
+                     non_block(new_fd);
+                     event.events = EPOLLIN | EPOLLET;
+                     event.data.fd = new_fd;
+                     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_fd, &event);
+                 }
+                 else
+                 {
+                     count = read(events[i].data.fd, read_buffer, sizeof(read_buffer)-1);
+                     read_buffer[count] = '\0';
+                     printf("%s \n", read_buffer);
+	 	     if(strcmp(read_buffer,"exit") == 0)
+                     {
+		         printf("Event close...\n");
+                         close(events[i].data.fd);
+                         break;
+                     }
+                 }
                  //close(events[i].data.fd);
         }
     }
