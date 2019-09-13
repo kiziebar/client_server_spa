d74ada1a7aa975d478cba22497c488bc38eb40d4
kiziebar <bartlomiej.kizielewicz@tieto.com>
2019-09-13 11:28:04 +0200

    correct client.c

--- tcp/client.c
+++ tcp/client.c
@@ -6,13 +6,13 @@
 #include <string.h>
 #include <unistd.h>
 
-#define PORT 8123
+const int PORT 8123;
 
 int main()
 {
     int sockfd;
     struct sockaddr_in servaddr;
-    char string[222];
+    char message[1024];
 
     // Socket create and varification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -33,9 +33,9 @@ int main()
     while(1)
     {
         printf("Podaj stringa \n");
-        scanf("%s", string);
-	    send(sockfd, string, strlen(string),0);
-        if(strcmp(string, "exit") == 0)
+        scanf("%s", message);
+        send(sockfd, message, strlen(message),0);
+        if(strcmp(message, "exit") == 0)
             break;
     }
 
